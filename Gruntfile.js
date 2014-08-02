01566947c5fdfe1b80f455162edd57f2fef8d0a4
Raymond Cheng <ryscheng@cs.washington.edu>
2014-08-02 14:09:25 -0700

    adding release tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,48 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jasmine_node: {
       integration: ['test']
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    },
+    'npm-publish': {
+      options: {
+        // list of tasks that are required before publishing
+        requires: [],
+        // if the workspace is dirty, abort publishing (to avoid publishing local changes)
+        abortIfDirty: true,
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-npm');
+  grunt.loadNpmTasks('grunt-bump');
+
   grunt.registerTask('test', [
   'jasmine_node'
   ]);
+  
+  grunt.registerTask('release', function(arg) {
+    if (arguments.length === 0) {
+      arg = 'patch';
+    }
+    grunt.task.run([
+      'bump:'+arg,
+      'npm-publish'
+    ]);
+  });
+
   grunt.registerTask('default', ['test']);
 };
